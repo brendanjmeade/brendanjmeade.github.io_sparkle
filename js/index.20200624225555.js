d5909d7d3eb302fb3535d9661dea4d9163987cf7
brendanjmeade <brendanjmeade@users.noreply.github.com>
2020-06-24 22:56:25 -0400

    Trying header

--- js/index.20200624225555.js
+++ js/index.20200624225555.js
@@ -3,7 +3,8 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/japangpsabstract-1594.png' : 'images/japangpsabstract-797.png');};
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/harvardlogo-140.jpg' : 'images/harvardlogo-70.jpg');
+$('.js2').attr('src', (dpi>1) ? 'images/japangpsabstract-1594.png' : 'images/japangpsabstract-797.png');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 initMenu($('#m1')[0]);

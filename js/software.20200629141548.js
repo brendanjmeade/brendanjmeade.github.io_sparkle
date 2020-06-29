289de3a7503056acec02f7b5f4958588ca16e744
Brendan Meade <brendanjmeade@gmail.com>
2020-06-29 14:16:19 -0400

    Continuing to fill in links on papers page

---
+++ js/software.20200629141548.js
@@ -0,0 +1,15 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js14').attr('src', (dpi>1) ? 'images/globalblocks-1034.jpg' : 'images/globalblocks-517.jpg');
+$('.js15').attr('src', (dpi>1) ? 'images/tderotated-1226.png' : 'images/tderotated-613.png');
+$('.js16').attr('src', (dpi>1) ? 'images/nn-1082.jpg' : 'images/nn-541.jpg');
+$('.js12').attr('src', (dpi>1) ? 'images/harvardlogo-140.jpg' : 'images/harvardlogo-70.jpg');
+$('.js13').attr('src', (dpi>1) ? 'images/ex_thrusttopo-1164.jpg' : 'images/ex_thrusttopo-582.jpg');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m5')[0]);
+
+});
